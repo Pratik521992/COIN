7b8688a897e0df183c8cb8daedbe44ca4d0a61a9
Ayush Kumar <pratik521992@gmail.com>
2021-05-16 21:25:41 +0530

    Dashboard upgrade

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,38 +1,35 @@
-import { Card } from "@material-ui/core";
 import React from "react";
 import CountUp from "react-countup";
 import { useSelector } from "react-redux";
 import { ethToInr, getETH } from "./utils/converted";
 import CardItem from "./utils/CardItem";
-import CURRENCY from '../currency.svg'
+import CURRENCY from "../currency.svg";
 
 function SearchBar() {
-  const { netWorth='', inr } = useSelector((state) => state.dashboard);
+  const { netWorth = "", inr } = useSelector((state) => state.dashboard);
   const ETH = getETH(netWorth);
   const value = ethToInr(inr, getETH(netWorth));
   return (
     <CardItem className="Search">
-            <Card>
-              <img className="currencyImage" src={CURRENCY} alt="Bitcoin" />
-            </Card>
-      <h3 style={{color: 'green'}} variant="h5" component="h2">
-            <CountUp
-              end={value}
-              separator=","
-              decimals={2}
-              decimal="."
-              suffix=" &#x20b9;"
-            />
-          </h3>
-      <h3 style={{color: 'green'}} variant="h5" component="h2">
-            <CountUp
-              end={ETH}
-              separator=" "
-              decimals={2}
-              decimal="."
-              suffix=" ETH"
-            />
-          </h3>
+      <img className="currencyImage" src={CURRENCY} alt="Bitcoin" />
+      <h3 style={{ color: "green" }} variant="h5" component="h2">
+        <CountUp
+          end={value}
+          separator=","
+          decimals={2}
+          decimal="."
+          suffix=" &#x20b9;"
+        />
+      </h3>
+      <h3 style={{ color: "green" }} variant="h5" component="h2">
+        <CountUp
+          end={ETH}
+          separator=" "
+          decimals={2}
+          decimal="."
+          suffix=" ETH"
+        />
+      </h3>
     </CardItem>
   );
 }
